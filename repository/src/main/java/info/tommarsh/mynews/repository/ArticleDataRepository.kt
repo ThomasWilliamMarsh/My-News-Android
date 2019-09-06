7cf8e7bc26339a9f9230f874b1f3718c88784b7e
Thomas Marsh <thomas.marsh@bbc.co.uk>
2019-09-06 16:01:25 +0100

    Use new coroutines flow for refreshing feed

--- repository/src/main/java/info/tommarsh/mynews/repository/ArticleDataRepository.kt
+++ repository/src/main/java/info/tommarsh/mynews/repository/ArticleDataRepository.kt
@@ -8,11 +8,11 @@ import info.tommarsh.mynews.core.model.CategoryModel
 import info.tommarsh.mynews.core.repository.ArticleRepository
 import info.tommarsh.mynews.repository.source.local.articles.ArticlesLocalDataStore
 import info.tommarsh.mynews.repository.source.remote.articles.ArticlesRemoteDataStore
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 typealias TopicOutcome = Pair<String, Outcome<List<ArticleModel>>>
@@ -24,7 +24,8 @@ class ArticleDataRepository
     override val errors: ErrorLiveData
 ) : ArticleRepository {
 
-    override suspend fun getBreakingNews(source: String): LiveData<List<ArticleModel>> = local.getBreakingNews()
+    override suspend fun getBreakingNews(source: String): LiveData<List<ArticleModel>> =
+        local.getBreakingNews()
 
     override suspend fun refreshBreakingNews() {
         when (val networkItems = remote.getBreakingNews()) {
@@ -38,26 +39,15 @@ class ArticleDataRepository
     override suspend fun searchArticles(query: String) = remote.searchArticles(query)
 
     override suspend fun refreshFeed(categories: List<CategoryModel>) = coroutineScope {
-        launch { local.deleteUnselectedCategories() }
-        articleProducer(categories).consumeEach { receiveArticles(it) }
-    }
-
-    private fun CoroutineScope.articleProducer(
-        categories: List<CategoryModel>
-    ) = produce {
-        categories.forEach {
-            send(produceArticles(it.id))
-        }
-    }
-
-    private suspend fun produceArticles(id: String): TopicOutcome {
-        return TopicOutcome(id, remote.getArticleForCategory(id))
-    }
-
-    private suspend fun receiveArticles(topic: TopicOutcome) {
-        when (val outcome = topic.second) {
-            is Outcome.Success -> local.saveCategory(topic.first, outcome.data)
-            is Outcome.Error -> errors.setError(outcome.error)
-        }
+        local.deleteUnselectedCategories()
+        categories.asFlow()
+            .map { TopicOutcome(it.id, remote.getArticleForCategory(it.id)) }
+            .buffer()
+            .collect {
+                when (val outcome = it.second) {
+                    is Outcome.Success -> local.saveCategory(it.first, outcome.data)
+                    is Outcome.Error -> errors.setError(outcome.error)
+                }
+            }
     }
 }
