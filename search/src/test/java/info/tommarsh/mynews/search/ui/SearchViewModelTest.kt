9613321e56db79f3e750bac6128369bf1915953c
thomaswilliammarsh <thomas.w.marsh@gmail.com>
2021-03-16 14:52:58 +0000

    Update paddings on recyclerview items

--- search/src/test/java/info/tommarsh/mynews/search/ui/SearchViewModelTest.kt
+++ search/src/test/java/info/tommarsh/mynews/search/ui/SearchViewModelTest.kt
@@ -1,19 +1,33 @@
 package info.tommarsh.mynews.search.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.paging.PagingData
+import com.nhaarman.mockitokotlin2.*
+import info.tommarsh.mynews.core.article.data.ArticleRepository
+import info.tommarsh.mynews.core.util.TimeHelper
+import info.tommarsh.mynews.search.model.MockModelProvider.articleModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class SearchViewModelTest {
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
     private val testCoroutineDispatcher = TestCoroutineDispatcher()
+
+    private val timeHelper = mock<TimeHelper>()
+
+    private val repository = mock<ArticleRepository>()
+
+    private val viewModel = SearchViewModel(repository, timeHelper)
 
     @Before
     fun `Set up`() {
@@ -24,5 +38,22 @@ class SearchViewModelTest {
     fun `Tear down`() {
         Dispatchers.resetMain()
         testCoroutineDispatcher.cleanupTestCoroutines()
+    }
+
+    @Test
+    fun `Fetches query from repository`() = runBlockingTest {
+        whenever(repository.searchArticles("Dogs")).thenReturn(
+            flowOf(
+                PagingData.from(
+                    listOf(
+                        articleModel
+                    )
+                )
+            )
+        )
+
+        viewModel.searchArticles("Dogs")
+
+        verify(repository).searchArticles("Dogs")
     }
 }
